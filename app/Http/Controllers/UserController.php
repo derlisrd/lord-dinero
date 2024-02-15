8f947ffa8f660084e3ff2e44f01daa01dfb2b9ab
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-02-15 10:08:20 -0300

    user update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     public function update(Request $request){
+       $validate =  Validator::make($request->all(),[
+
+       ]);
 
         return response()->json([
             'success'=>true,
